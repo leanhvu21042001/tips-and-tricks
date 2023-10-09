211e9b38c05fb07789df2c83c02c0dd04c9f7954
leanhvu21042001 <leanhvu21042001@gmail.com>
2023-10-09 21:22:56 +0700

    fix: change author all commits

--- change_author_all_commits_work_good.sh
+++ change_author_all_commits_work_good.sh
@@ -1,19 +1,16 @@
 #!/bin/sh
-  2
-  3 git filter-branch --env-filter '
-  4
-  5 OLD_EMAIL="your-old-email@example.com"
-  6 CORRECT_NAME="Your Correct Name"
-  7 CORRECT_EMAIL="your-correct-email@example.com"
-  8
-  9 if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
- 10 then
- 11     export GIT_COMMITTER_NAME="$CORRECT_NAME"
- 12     export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
- 13 fi
- 14 if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
- 15 then
- 16     export GIT_AUTHOR_NAME="$CORRECT_NAME"
- 17     export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
- 18 fi
- 19 ' --tag-name-filter cat -- --branches --tags
+git filter-branch --env-filter '
+OLD_EMAIL="vu.la@primecommerce.edu.vn"
+CORRECT_NAME="leanhvu21042001"
+CORRECT_EMAIL="leanhvu21042001@gmail.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
